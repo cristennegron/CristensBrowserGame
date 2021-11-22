3053c106001b3b45c1d84a6b8c7c700f6bebf0ef
Cristen Negron <cristy.negron@gmail.com>
2021-11-22 13:03:22 -0500

    made the snake and apple smaller by adding more divs and changing the width/height of the board divs and the const width in the JS

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const squares = document.querySelectorAll('.board div')
 const scoreDisplay = document.querySelector('span')
 const startBtn = document.querySelector('.start')
 
-const width = 10
+const width = 20
 let currentIndex = 0 
 let appleIndex = 0 
-let currentSnake = [2,1,0] 
+let snakePosition = [2,1,0] 
 let direction = 1
 let score = 0
 let speed = 0.9
@@ -13,40 +13,40 @@ let intervalTime = 0
 let interval = 0
 
 function startGame() {
-    currentSnake.forEach(index => squares[index].classList.remove('snake'))
+    snakePosition.forEach(index => squares[index].classList.remove('snake'))
     squares[appleIndex].classList.remove('apple')
     clearInterval(interval)
     score = 0
     randomApple()
     direction = 1
     scoreDisplay.innerText = score
-    intervalTime = 1000
-    currentSnake = [2,1,0]
+    intervalTime = 400
+    snakePosition = [2,1,0]
     currentIndex = 0
-    currentSnake.forEach(index => squares[index].classList.add('snake'))
+    snakePosition.forEach(index => squares[index].classList.add('snake'))
     interval = setInterval(moveOutcomes, intervalTime)
   }
 
 function moveOutcomes() {
     if (
-      (currentSnake[0] + width >= (width * width) && direction === width ) || 
-      (currentSnake[0] % width === width -1 && direction === 1) || 
-      (currentSnake[0] % width === 0 && direction === -1) || 
-      (currentSnake[0] - width < 0 && direction === -width) ||  
-      squares[currentSnake[0] + direction].classList.contains('snake') 
+      (snakePosition[0] + width >= (width * width) && direction === width) || 
+      (snakePosition[0] % width === width -1 && direction === 1) || 
+      (snakePosition[0] % width === 0 && direction === -1) || 
+      (snakePosition[0] - width < 0 && direction === -width) ||  
+      squares[snakePosition[0] + direction].classList.contains('snake') 
     ) {
-      return clearInterval(interval) 
+        return clearInterval(interval) 
     }
 
-    const tail = currentSnake.pop() 
+    const tail = snakePosition.pop() 
     squares[tail].classList.remove('snake')  
-    currentSnake.unshift(currentSnake[0] + direction)
+    snakePosition.unshift(snakePosition[0] + direction)
 
 
-if(squares[currentSnake[0]].classList.contains('apple')) {
-    squares[currentSnake[0]].classList.remove('apple')
+if(squares[snakePosition[0]].classList.contains('apple')) {
+    squares[snakePosition[0]].classList.remove('apple')
     squares[tail].classList.add('snake')
-    currentSnake.push(tail)
+    snakePosition.push(tail)
     randomApple()
     score++
     scoreDisplay.textContent = score
@@ -54,29 +54,35 @@ if(squares[currentSnake[0]].classList.contains('apple')) {
     intervalTime = intervalTime * speed
     interval = setInterval(moveOutcomes, intervalTime)
   }
-  squares[currentSnake[0]].classList.add('snake')
+  squares[snakePosition[0]].classList.add('snake')
   }
 
-  function randomApple() {
+function randomApple() {
     do{
       appleIndex = Math.floor(Math.random() * squares.length)
     } while(squares[appleIndex].classList.contains('snake')) 
     squares[appleIndex].classList.add('apple')
   }
 
-function control(event) {
+const headPosition = snakePosition[0];
+const snakeHead = squares[headPosition];
 
-    if(event.code === 'ArrowRight') {
-      direction = 1 
+function control(event) {
+  
+  if(event.code === 'ArrowRight') {
+      direction = 1
+      
     } else if (event.code === 'ArrowUp') {
       direction = -width 
+      
     } else if (event.code === 'ArrowLeft') {
-      direction = -1 
+      direction = -1
+      
     } else if (event.code === 'ArrowDown') {
       direction = +width 
     }
   }
-  
+
   document.addEventListener('keyup', control)
   startBtn.addEventListener('click', startGame)
 
