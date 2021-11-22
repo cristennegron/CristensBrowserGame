2438489a66f8436c4f211b86730bbeb636c80797
Cristen Negron <cristy.negron@gmail.com>
2021-11-21 19:39:14 -0500

    created function to start and restart the game with our snake positioning and movement

--- app.js
+++ app.js
@@ -12,18 +12,17 @@ let speed = 0.9
 let intervalTime = 0
 let interval = 0
 
-
 function startGame() {
-  currentSnake.forEach(index => squares[index].classList.remove('snake'))
-  squares[appleIndex].classList.remove('apple')
-  clearInterval(interval)
-  score = 0
-  randomApple()
-  direction = 1
-  scoreDisplay.innerText = score
-  intervalTime = 1000
-  currentSnake = [2,1,0]
-  currentIndex = 0
-  currentSnake.forEach(index => squares[index].classList.add('snake'))
-  interval = setInterval(moveOutcomes, intervalTime)
-}
+    currentSnake.forEach(index => squares[index].classList.remove('snake'))
+    squares[appleIndex].classList.remove('apple')
+    clearInterval(interval)
+    score = 0
+    randomApple()
+    direction = 1
+    scoreDisplay.innerText = score
+    intervalTime = 1000
+    currentSnake = [2,1,0]
+    currentIndex = 0
+    currentSnake.forEach(index => squares[index].classList.add('snake'))
+    interval = setInterval(moveOutcomes, intervalTime)
+  }
